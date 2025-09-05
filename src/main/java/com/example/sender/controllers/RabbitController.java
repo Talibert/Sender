a0d4b54927484a5fc3293f72061a22f86816d77d
Talibert <guilhermetaliberti@gmail.com>
2025-09-05 00:57:02 -0300

    Configurando aplicação para conectar com o rabbit e enviar mensagens para a fila

---
+++ src/main/java/com/example/sender/controllers/RabbitController.java
@@ -0,0 +1,25 @@
+package com.example.sender.controllers;
+
+import com.example.sender.service.RabbitSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Esse controller é para testes de comunicação entre os dois projetos
+ */
+@RestController
+@RequestMapping(value = "/rabbitTest")
+public class RabbitController {
+
+    @Autowired
+    private RabbitSenderService rabbitSenderService;
+
+    @GetMapping("/send")
+    public String enviar(@RequestParam String mensagem) {
+        rabbitSenderService.sendMessage(mensagem);
+        return "Mensagem enviada: " + mensagem;
+    }
+}
---
+++ src/main/java/com/example/sender/service/RabbitSenderService.java
@@ -0,0 +1,19 @@
+package com.example.sender.service;
+
+import com.example.sender.config.RabbitConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitSenderService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitSenderService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String mensagem) {
+        rabbitTemplate.convertAndSend(RabbitConfig.MY_QUEUE, mensagem);
+    }
+}
