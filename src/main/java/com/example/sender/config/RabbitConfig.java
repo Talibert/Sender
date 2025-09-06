9f4a8636e87ff3a8507b48955aae01f13e0d7066
Talibert <guilhermetaliberti@gmail.com>
2025-09-05 23:53:45 -0300

    Modificando o projeto para receber respostas processadas do receiver

--- src/main/java/com/example/sender/config/RabbitConfig.java
+++ src/main/java/com/example/sender/config/RabbitConfig.java
@@ -4,13 +4,25 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Classe responsável por criar os Beans de cada fila e registrar no contexto Spring
+ * O Spring AMQP cria a fila no RabbitMQ assim que a aplicação sobe
+ *
+ * Portanto, basta chamar os métodos do rabbit utilizando as filas declaradas
+ */
 @Configuration
 public class RabbitConfig {
 
-    public static final String MY_QUEUE = "my-queue";
+    public static final String REQUEST_QUEUE = "request-queue";
+    public static final String RESPONSE_QUEUE = "response-queue";
 
     @Bean
-    public Queue myQueue() {
-        return new Queue(MY_QUEUE, true);
+    public Queue requestQueue() {
+        return new Queue(REQUEST_QUEUE, true);
+    }
+
+    @Bean
+    public Queue responseQueue() {
+        return new Queue(RESPONSE_QUEUE, true);
     }
 }
--- src/main/java/com/example/sender/service/RabbitSenderService.java
+++ src/main/java/com/example/sender/service/RabbitSenderService.java
@@ -1,10 +1,14 @@
 package com.example.sender.service;
 
 import com.example.sender.config.RabbitConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service para gerenciar envio e recebimento de mensagens
+ */
 @Service
 public class RabbitSenderService {
 
@@ -15,7 +19,20 @@ public class RabbitSenderService {
         this.rabbitTemplate = rabbitTemplate;
     }
 
+    /**
+     * Envia a mensagem para o segundo projeto através da fila REQUES_QUEUE
+     * @param mensagem
+     */
     public void sendMessage(String mensagem) {
-        rabbitTemplate.convertAndSend(RabbitConfig.MY_QUEUE, mensagem);
+        rabbitTemplate.convertAndSend(RabbitConfig.REQUEST_QUEUE, mensagem);
+    }
+
+    /**
+     * Vai assinar a fila RESPONSE_QUEUE para receber as mensagens processadas do segundo projeto
+     * @param resposta
+     */
+    @RabbitListener(queues = RabbitConfig.RESPONSE_QUEUE)
+    public void receiveResponse(String resposta) {
+        System.out.println("Resposta processada pelo segundo projeto: " + resposta);
     }
 }
